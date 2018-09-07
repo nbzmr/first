eb5069fd3fafe00e10dbd983e77b364636172f9d
unknown <jklfaeifncaslkfj>
2018-09-07 11:17:09 -0700

    fetch module removed

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const hbs = require('hbs')
 const fs = require('fs')
-const fetch = require('node-fetch')
 
 const app = express()
 const port = process.env.PORT || 3000
