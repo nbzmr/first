6bb7cade8e131c803dec380c9ea71fc76ca67e46
unknown <jklfaeifncaslkfj>
2018-09-07 10:40:15 -0700

    nice

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require('express')
 const hbs = require('hbs')
 const fs = require('fs')
-const axios = require('axios')
-const fetch = require('node-fetch')
 
 const app = express()
 const port = process.env.PORT || 3000
